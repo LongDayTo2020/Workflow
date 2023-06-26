81d448d615897f83b5b4dc4d3924943db6937454
Lanx <Lanx@rgaming168.com>
2023-06-26 14:12:55 +0800

    Add: File library

---
+++ Workflow.Library/FileLibrary.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Workflow.Library;
+
+public static class FileLibrary
+{
+    public static async Task<string> SaveFileAsync(IFormFile file, string directoryPath)
+    {
+        // Generate a unique file name or use the original file name
+        string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+
+        // Combine the directory path and the unique file name
+        string filePath = Path.Combine(directoryPath, uniqueFileName);
+
+        // Save the file to the specified path
+        await using var fileStream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(fileStream);
+
+        // Return the file path or any other information you need
+        return filePath;
+    }
+}
